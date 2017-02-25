078266501e71834094f20f3e18595926010bb6d8
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-02-25 00:14:53 -0800

    Add files via upload

---
+++ include/ExN03EventActionMessenger.hh
@@ -0,0 +1,31 @@
+
+#ifndef ExN03EventActionMessenger_h
+#define ExN03EventActionMessenger_h 1
+
+#include "globals.hh"
+#include "G4UImessenger.hh"
+
+#include "G4SystemOfUnits.hh"
+#include "G4PhysicalConstants.hh"
+#include "G4UnitsTable.hh"
+
+class ExN03EventAction;
+class G4UIcmdWithAString;
+class G4UIcmdWithAnInteger;
+
+
+class ExN03EventActionMessenger: public G4UImessenger
+{
+  public:
+    ExN03EventActionMessenger(ExN03EventAction*);
+   ~ExN03EventActionMessenger();
+    
+    void SetNewValue(G4UIcommand*, G4String);
+    
+  private:
+    ExN03EventAction*   eventAction;   
+    G4UIcmdWithAString* DrawCmd;
+    G4UIcmdWithAnInteger* PrintCmd;    
+};
+
+#endif
