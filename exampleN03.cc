344caaea44f60a81c43f7033dc96cfa5a56abedc
Yaro Kaminskiy <yaroslavk@berkeley.edu>
2017-03-30 17:24:28 -0700

    Add files via upload

--- exampleN03.cc
+++ exampleN03.cc
@@ -20,14 +20,20 @@
 // 
 // --------------------------------------------------------------
 
+#ifdef G4MULTITHREADED
+#include "G4MTRunManager.hh"
+#else
 #include "G4RunManager.hh"
+#endif
+
 #include "G4UImanager.hh"
-#include "G4UIterminal.hh"
-#include "G4UIExecutive.hh"
-#include "Randomize.hh"
 
 #ifdef G4VIS_USE
-#include "ExN03VisManager.hh"
+#include "G4VisExecutive.hh"
+#endif
+
+#ifdef G4UI_USE
+#include "G4UIExecutive.hh"
 #endif
 
 #include "ExN03DetectorConstruction.hh"
@@ -44,28 +50,22 @@ int main(int argc,char** argv) {
   CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine);
    
   // Construct the default run manager
-  G4RunManager * runManager = new G4RunManager;
+#ifdef G4MULTITHREADED
+  G4MTRunManager* runManager = new G4MTRunManager;
+  //runManager->SetNumberOfThreads(2); 
+#else
+  G4RunManager* runManager = new G4RunManager;
+#endif
 
   // set mandatory initialization classes
   ExN03DetectorConstruction* detector = new ExN03DetectorConstruction;
   runManager->SetUserInitialization(detector);
   runManager->SetUserInitialization(new ExN03PhysicsList);
   
- G4UIsession* session=0;
-  
-  if (argc==1)   // Define UI session for interactive mode.
-    {
-      // G4UIterminal is a (dumb) terminal.
-//#ifdef G4UI_USE_XM
-//      session = new G4UIXm(argc,argv);
-//#else
-      session = new G4UIterminal;
-//#endif
-    }
-  
+  // Set visualization and user interface
 #ifdef G4VIS_USE
-  // visualization manager
-  G4VisManager* visManager = new ExN03VisManager;
+  // Visualization manager
+  G4VisManager* visManager = new G4VisExecutive;
   visManager->Initialize();
 #endif
     
@@ -77,9 +77,52 @@ int main(int argc,char** argv) {
  
   //Initialize G4 kernel
   runManager->Initialize();
-    
-  // get the pointer to the User Interface manager 
-  G4UImanager* UI = G4UImanager::GetUIpointer();  
+
+  // Get the pointer to the UI manager
+  G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+  // Define UI session for interactive mode.
+  if(argc == 1)
+    {
+#ifdef G4UI_USE
+      G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+#ifdef G4VIS_USE
+      UImanager->ApplyCommand("/control/execute prerunN03.mac");     
+#endif
+      ui->SessionStart();
+      delete ui;
+#endif
+    }
+  // Batch mode
+  else
+    { 
+      G4String command = "/control/execute ";
+      G4String fileName = argv[1];
+      UImanager->ApplyCommand(command+fileName);
+    }
+  
+
+//   if (argc!=1)   // batch mode
+//     {
+//       G4String command = "/control/execute ";
+//       G4String fileName = argv[1];
+//       UImanager->ApplyCommand(command+fileName);
+//     }
+//   else // interactive mode
+//     {
+// #ifdef G4UI_USE
+//       G4UIExecutive* ui = new G4UIExecutive(argc, argv);
+//       if (ui->IsGUI())
+//   {
+//     /* prerunN03.mac is loaded by default */
+//     UImanager->ApplyCommand("/control/execute prerunN03.mac");
+//     ui->SessionStart();
+//   }
+//       delete ui;
+// #endif
+
+/*  // get the pointer to the User Interface manager 
+  G4UImanager* UI = G4UImanager::GetUIpointer();
 
   if (session)   // Define UI session for interactive mode.
     {
@@ -93,7 +136,7 @@ int main(int argc,char** argv) {
       G4String command = "/control/execute ";
       G4String fileName = argv[1];
       UI->ApplyCommand(command+fileName);
-    }
+    }*/
 
   // job termination
 #ifdef G4VIS_USE
